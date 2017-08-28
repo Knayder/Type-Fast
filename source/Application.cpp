3f59ff76db302cbfc65f51c716c87209ba533a48
Knayder <knayder@outlook.com>
2017-08-29 01:41:05 +0200

    Merged with Input-Manager branch

--- source/Application.cpp
+++ source/Application.cpp
@@ -4,7 +4,7 @@ Application::Application() :
 	window(sf::VideoMode(1280, 720), "Type Fast")
 {
 	stateManager.push(std::make_unique<GameState>(sf::Color(137, 137, 148)));
-
+	//do something
 }
 
 void Application::run() {
